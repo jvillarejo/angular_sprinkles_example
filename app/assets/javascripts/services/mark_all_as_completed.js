f513066b6559d3d6979e11183d825ee4632557c4
jvillarejo <arzivian87@gmail.com>
2014-10-03 13:10:11 -0300

    changed to update frontend model aftre success

--- app/assets/javascripts/services/mark_all_as_completed.js
+++ app/assets/javascripts/services/mark_all_as_completed.js
@@ -1,9 +1,9 @@
 sprinkles.service('markAllAsCompleted', ['$http', function ($http) {
   return function (todos) {
-    todos.forEach(function (todo) {
-      todo.complete = true;
+    $http.put('/todos/all_completed.json').then(function() {
+        todos.forEach(function (todo) {
+        todo.complete = true;
+      });
     });
-
-    $http.put('/todos/all_completed.json');
   };
 }]);
